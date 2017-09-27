8681a30f371b3268595694ab2d33c78fb0e8a609
Hydrogen <fanyangliang@gmail.com>
2017-09-27 22:32:11 +0800

    update hQuery

--- hQuery.js
+++ hQuery.js
@@ -15,9 +15,15 @@
     H.prototype = {
         constructor: H,
         length: 0,
+        prevObject: null,
 
         init: function (selector, context) {
             if (!selector) {
+                return this
+            }
+
+            if (selector.constructor === H) {
+                Object.assign(this, selector)
                 return this
             }
 
@@ -30,6 +36,7 @@
 
             context = context || d
             let nodes = context.querySelectorAll(selector)
+
             Object.assign(this, nodes)
             this.length = nodes.length
         }
@@ -40,77 +47,193 @@
     w.hQuery = H
 
 
-    // html methods
+    // selector methods
     H.prototype.find = function (selector) {
-        return new H(selector, this[0])
+        let ret = new H(selector, this[0])
+        ret.prevObject = this
+        return ret
     }
 
     H.prototype.first = function () {
-        return new H(this[0])
+        let ret = new H(this[0])
+        ret.prevObject = this
+        return ret
     }
 
     H.prototype.last = function () {
-        return new H(this[this.length-1])
+        let ret = new H(this[this.length-1])
+        ret.prevObject = this
+        return ret
     }
 
     H.prototype.eq = function (num) {
-        num = num < 0 ? num : this.length - num
-        return new H(this[num])
+        num = num > 0 ? num : this.length - num
+        let ret = new H(this[num])
+        ret.prevObject = this
+        return ret
     }
 
-    // todo: if this[0] is undefined ?
-    H.prototype.html = function (html) {
-        if (!html) {
-            return this[0].innerHTML
+    H.prototype.parent = function () {
+        let ret = new H(this[0].parentNode)
+        ret.prevObject = this
+        return ret
+    }
+
+    H.prototype.children = function (num) {
+        let ret;
+        let children = this[0].children
+        if (num !== undefined) {
+            num = num > 0 ? num : children.length - num
+            ret = new H(children[num])
+        } else {
+            ret = new H()
+            for (let i = 0; i < children.length; i++) {
+                ret[i] = children[i]
+            }
+            ret.length = children.length
         }
-        this[0].innerHTML = html
+        ret.prevObject = this
+        return ret
+    }
+
+    H.prototype.next = function () {
+        let index = 0;
+        let ret = new H()
+        for (let i = 0; i < this.length; i++) {
+            let sibling = this[i].nextSibling
+            // ignore TEXT_NODE
+            while (sibling && sibling.nodeType === 3) {
+                sibling = sibling.nextSibling
+            }
+            if (sibling) {
+                ret[index++] = sibling
+            }
+        }
+        ret.length = index
+        ret.prevObject = this
+        return ret
+    }
+
+    H.prototype.prev = function () {
+        let index = 0;
+        let ret = new H()
+        for (let i = 0; i < this.length; i++) {
+            let sibling = this[i].previousSibling
+            while (sibling && sibling.nodeType === 3) {
+                sibling = sibling.previousSibling
+            }
+            if (sibling) {
+                ret[index++] = sibling
+            }
+        }
+        ret.length = index
+        ret.prevObject = this
+        return ret
+    }
+
+    H.prototype.filter = function (fn) {
+        let index = 0;
+        let ret = new H()
+        for (let i = 0; i < this.length; i++) {
+            let val = fn.call(this[i], i, this[i])
+            if (val) {
+                ret[index++] = this[i]
+            }
+        }
+        ret.length = index
+        ret.prevObject = this
+        return ret
+    }
+
+    H.prototype.end = function () {
+        return this.prevObject
+    }
+
+    // html methods
+    H.prototype.html = function (html) {
+        for (let i = 0; i < this.length; i++) {
+            if (html === undefined) {
+                return this[0].innerHTML
+            }
+            this[i].innerHTML = html
+        }
         return this
     }
 
     H.prototype.text = function (text) {
-        if (!text) {
-            return this[0].textContent
+        for (let i = 0; i < this.length; i++) {
+            if (text === undefined) {
+                return this[0].textContent
+            }
+            this[i].textContent = text
         }
-        this[0].textContent  = text
+        return this
+    }
+
+    H.prototype.val = function (val) {
+        for (let i = 0; i < this.length; i++) {
+            if (val === undefined) {
+                return this[0].value
+            }
+            this[i].value = val
+        }
         return this
     }
 
     H.prototype.attr = function (attr, val) {
-        if (!val) {
-            return this[0].getAttribute(attr)
-        }
         for (let i = 0; i < this.length; i++) {
-            this[i].setAttribute(attr, val)
+            if (typeof attr === 'string') {
+                if (arguments.length === 1) {
+                    return this[0].getAttribute(attr)
+                }
+                this[i].setAttribute(attr, val)
+            } else {
+                for (let a in attr) {
+                    this[i].setAttribute(a, attr[a])
+                }
+            }
         }
         return this
     }
 
     H.prototype.append = function (html) {
-        if (typeof html === 'string') {
-            let temp = document.createElement('div');
-            temp.innerHTML = html;
-            let frag = document.createDocumentFragment();
-            while (temp.firstChild) {
-                frag.appendChild(temp.firstChild);
+        for (let i = 0; i < this.length; i++) {
+            if (typeof html === 'string') {
+                this[i].insertAdjacentHTML('beforeend', html)
             }
-            this[0].appendChild(frag);
+            else {
+                this[i].append(...arguments);
+            }
         }
-        else {
-            this[0].appendChild(html);
+        return this
+    }
+    
+    H.prototype.prepend = function (html) {
+        for (let i = 0; i < this.length; i++) {
+            if (typeof html === 'string') {
+                this[i].insertAdjacentHTML('afterbegin', html)
+            }
+            else {
+                this[i].prepend(...arguments);
+            }
         }
         return this
     }
 
     H.prototype.remove = function () {
         for (let i = 0; i < this.length; i++) {
-            this[i].parentNode.removeChild(this[i])
+            this[i].remove()
         }
         return this
     }
 
     H.prototype.replaceWith = function (html) {
         for (let i = 0; i < this.length; i++) {
-            this[i].outerHTML = html
+            if (typeof html === 'string') {
+                this[i].outerHTML = html
+            } else {
+                this[i].replaceWith(...arguments)
+            }
         }
         return this
     }
@@ -140,10 +263,16 @@
     // css methods
     H.prototype.css = function (attr, val) {
         for (let i = 0; i < this.length; i++) {
-            if (val) {
-                return w.getComputedStyle(this[i], null)[attr]
+            if (typeof attr === 'string') {
+                if (arguments.length === 1) {
+                    return w.getComputedStyle(this[0], null)[attr]
+                }
+                this[i].style[attr] = val
+            } else {
+                for (let a in attr) {
+                    this[i].style[a] = attr[a]
+                }
             }
-            this[i].style[attr] = val
         }
         return this
     }
@@ -179,32 +308,39 @@
     }
 
     // Event Listener methods
-    H.prototype.on = function (eventName, eventHandler) {
+    H.prototype.on = function (event, handler) {
         for (let i = 0; i < this.length; i++) {
-            this[i].addEventListener(eventName, eventHandler);
+            this[i].addEventListener(event, handler);
         }
         return this
     }
 
-    H.prototype.off = function (eventName, eventHandler) {
+    H.prototype.off = function (event, handler) {
         for (let i = 0; i < this.length; i++) {
-            this[i].removeEventListener(eventName, eventHandler);
+            this[i].removeEventListener(event, handler);
         }
         return this
     }
 
 
     // other methods
-    H.prototype.each = function (callback) {
+    H.prototype.each = function (fn) {
         for (let i = 0; i < this.length; i++) {
-            let val = callback.call(this[i], i, this[i])
+            let val = fn.call(this[i], i, this[i])
             if (val === false) break
         }
         return this
     }
 
-    H.prototype.extend = function (name, func) {
-        H.prototype[name] = func
+    // extend method on instance or prototype
+    H.extend = H.prototype.extend = function (name, fn) {
+        if (arguments.length === 2) {
+            H.prototype[name] = fn
+        } else {
+            for (let n in name) {
+                H.prototype[n] = name[n]
+            }
+        }
     }
 
 })(window, document);
